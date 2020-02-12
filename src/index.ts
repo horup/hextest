ec20aa57553399a7fe7ce8f0c3818edd38000422
horup <sah@sah.dk>
2020-02-12 08:54:58 +0100

    added working hex map using some hacks

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,43 @@
 import * as PIXI from 'pixi.js';
-import { defineGrid } from 'honeycomb-grid';
+import * as Honycomb from 'honeycomb-grid';
+
+// https://www.redblobgames.com/grids/hexagons/
+// https://www.npmjs.com/package/honeycomb-grid
+
 declare var require;
 const flat = require("../images/nogrid/hexset_grid_boreal_flat_01.png");
 const app = new PIXI.Application();
-// The application will create a canvas element for you that you
-// can then insert into the DOM.
+
 document.body.appendChild(app.view);
 
 
+const Grid = Honycomb.defineGrid();
+const grid = Grid.rectangle({width:24, height:24});
+
+const gridContainer = new PIXI.Container;
+app.stage.addChild(gridContainer);
+
+const sprites = new Map<object, PIXI.Sprite>(); 
 
 app.ticker.add(()=>{
-
+    grid.forEach(hex=>
+    {
+        if (!sprites.has(hex))
+        {
+            let s = new PIXI.Sprite(PIXI.Texture.from(flat));
+            gridContainer.addChild(s);
+            sprites.set(hex, s);
+        }
+        else
+        {
+            let w = 18;
+            let h = 14;
+            let p = hex.toPoint();
+            let s = sprites.get(hex);
+            s.position.x = p.x * w;
+            s.position.y = p.y * h;
+        }
+    });
 });
 
 //PIXI.Loader.shared.add("hex", "hex")
