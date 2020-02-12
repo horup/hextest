1b876ca5a0aa40e8a3b30b2b278fb18672a7cef5
horup <sah@sah.dk>
2020-02-12 10:38:42 +0100

    added flat hex + soldier sprite

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,27 @@ import * as Honycomb from 'honeycomb-grid';
 // https://www.redblobgames.com/grids/hexagons/
 // https://www.npmjs.com/package/honeycomb-grid
 
+PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
+
 declare var require;
-const flat = require("../images/nogrid/hexset_grid_boreal_flat_01.png");
+const flat = require("../images/hexes/grass.png");
+const soldiers = require("../images/units/soldiers.png");
 const app = new PIXI.Application();
 
 document.body.appendChild(app.view);
 
+const Hex = Honycomb.extendHex({
+    orientation:'flat',
+    size:0.5
+});
+const Grid = Honycomb.defineGrid(Hex);
+const grid = Grid.rectangle({width:33, height:21});
 
-const Grid = Honycomb.defineGrid();
-const grid = Grid.rectangle({width:24, height:24});
-
-const gridContainer = new PIXI.Container;
+app.stage.scale.set(32);
+const gridContainer = new PIXI.Container();
+const unitContainer = new PIXI.Container();
 app.stage.addChild(gridContainer);
+app.stage.addChild(unitContainer);
 
 const sprites = new Map<object, PIXI.Sprite>(); 
 
@@ -27,11 +36,13 @@ app.ticker.add(()=>{
             let s = new PIXI.Sprite(PIXI.Texture.from(flat));
             gridContainer.addChild(s);
             sprites.set(hex, s);
+            s.width = 1;
+            s.height = 1;
         }
         else
         {
-            let w = 18;
-            let h = 14;
+            let w = 1;
+            let h = 1;
             let p = hex.toPoint();
             let s = sprites.get(hex);
             s.position.x = p.x * w;
@@ -39,6 +50,26 @@ app.ticker.add(()=>{
         }
     });
 });
+
+{
+    let s = new PIXI.Sprite(PIXI.Texture.from(soldiers));
+    s.width = 1;
+    s.height = 1;
+    unitContainer.addChild(s);
+
+    let p = grid.get({x:1, y:1}).toPoint();
+    s.position.set(p.x, p.y);
+}
+
+{
+    let s = new PIXI.Sprite(PIXI.Texture.from(soldiers));
+    s.width = 1;
+    s.height = 1;
+    unitContainer.addChild(s);
+
+    let p = grid.get({x:3, y:2}).toPoint();
+    s.position.set(p.x, p.y);
+}
 
 //PIXI.Loader.shared.add("hex", "hex")
  /*
